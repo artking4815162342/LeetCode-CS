4b1183a1be6e5742199aed97573e50ea27b0ed41
Artur Kornienko <artur.kornienko@corp.mail.ru>
2023-03-04 18:02:38 +0100

    0189-RotateArray

--- LeetCode/0151-0200/0189-RotateArray.cs
+++ LeetCode/0151-0200/0189-RotateArray.cs
@@ -1,6 +1,8 @@
 //-----------------------------------------------------------------------------
 // Runtime: 232ms
+// Runtime: 159ms
 // Memory Usage: 32.8 MB
+// Memory Usage: 70.8 MB
 // Link: https://leetcode.com/submissions/detail/409217846/
 //-----------------------------------------------------------------------------
 
@@ -10,26 +12,85 @@ namespace LeetCode
     {
         public void Rotate(int[] nums, int k)
         {
-            if (k >= nums.Length)
-                k %= nums.Length;
+            var list = ArrayToList(nums);
+            var resultList = RotateRight(list, k);
+            ListToArrayBuffer(resultList, nums);
+        }
 
-            for (int start = 0, count = 0; start < k && count < nums.Length; start++)
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            if (head == null || k <= 0)
             {
-                int curr = start;
-                var prev = nums[curr];
-                do
-                {
-                    var next = curr + k;
-                    if (next >= nums.Length)
-                        next -= nums.Length;
+                return head;
+            }
 
-                    var temp = nums[next];
-                    nums[next] = prev;
-                    prev = temp;
-                    curr = next;
+            var listInfo = GetListInfo(head, 0);
 
-                    count++;
-                } while (start != curr);
+            k %= listInfo.cout;
+
+            if (listInfo.cout <= 1 || k == 0)
+            {
+                return head;
+            }
+
+            var index = 0;
+            var target = listInfo.cout - k;
+            var node = head;
+            while (index < target - 1)
+            {
+                node = node.next;
+                index++;
+            }
+
+            var result = node.next;
+            listInfo.tail.next = head;
+            node.next = null;
+
+            return result;
+        }
+
+        private (int cout, ListNode tail) GetListInfo(ListNode head, int count)
+        {
+            if (head.next == null)
+            {
+                return (count + 1, head);
+            }
+
+            return GetListInfo(head.next, count + 1);
+        }
+
+        private static ListNode ArrayToList(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return null;
+            }
+
+            var head = new ListNode(nums[0]);
+            var node = head;
+
+            if (nums.Length == 1)
+            {
+                return head;
+            }
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                node.next = new ListNode(nums[i]);
+                node = node.next;
+            }
+
+            return head;
+        }
+
+        private static void ListToArrayBuffer(ListNode head, int[] buffer)
+        {
+            var index = 0;
+            while (head != null)
+            {
+                buffer[index] = head.val;
+                index++;
+                head = head.next;
             }
         }
     }
---
+++ LeetCode/Utility.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode
+{
+    internal static class Utility
+    {
+        private static ListNode ArrayToList(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return null;
+            }
+
+            var head = new ListNode(nums[0]);
+            var node = head;
+
+            if (nums.Length == 1)
+            {
+                return head;
+            }
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                node.next = new ListNode(nums[i]);
+                node = node.next;
+            }
+
+            return head;
+        }
+
+        private static void ListToArrayBuffer(ListNode head, int[] buffer)
+        {
+            var index = 0;
+            while (head != null)
+            {
+                buffer[index] = head.val;
+                index++;
+                head = head.next;
+            }
+        }
+    }
+}
