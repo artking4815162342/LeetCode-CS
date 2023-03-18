e0b45ba45b032c3246111691b87c2739b40a6236
Artur Kornienko <artur.kornienko@corp.mail.ru>
2023-03-18 22:49:11 +0100

    028-ImplementStrStr

--- LeetCode/0001-0050/028-ImplementStrStr.cs
+++ LeetCode/0001-0050/028-ImplementStrStr.cs
@@ -11,28 +11,35 @@ namespace LeetCode
         public int StrStr(string haystack, string needle)
         {
             var needleLength = needle.Length;
-            if (needleLength == 0) { return 0; }
+            if (needleLength == 0)
+            {
+                return 0;
+            }
 
             var loopLenght = haystack.Length - needleLength + 1;
-            var needleIndex = 0;
-            var n = 0;
-            for (int i = 0; i < loopLenght; i++)
+            for (var i = 0; i < loopLenght; i++)
             {
-                needleIndex = n = 0;
-                while (needleIndex < needleLength
-                    && haystack[i + needleIndex] == needle[needleIndex])
+                int n;
+                var needleIndex = n = 0;
+                while (needleIndex < needleLength && haystack[i + needleIndex] == needle[needleIndex])
                 {
                     if (n == 0 && needle[needleIndex] == needle[0])
                     {
                         n = needleIndex;
                     }
+
                     needleIndex++;
                 }
+
                 if (needleIndex == needleLength)
                 {
                     return i;
                 }
-                if (n > 0) i += n - 1;
+
+                if (n > 0)
+                {
+                    i += n - 1;
+                }
             }
 
             return -1;
