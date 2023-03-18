8f90ad57649bcfe52c2d7375741eac6fcb0f2bae
Artur Kornienko <artur.kornienko@corp.mail.ru>
2023-03-19 00:41:41 +0100

    012-IntegerToRoman

--- LeetCode/0001-0050/012-IntegerToRoman.cs
+++ LeetCode/0001-0050/012-IntegerToRoman.cs
@@ -10,11 +10,20 @@ namespace LeetCode
 {
     public class _012_IntegerToRoman
     {
+        private readonly string[] symbol =
+        {
+            "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX", "L",
+            "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"
+        };
+
+        private readonly int[] value =
+        {
+            3000, 2000, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 9, 8, 7,
+            6, 5, 4, 3, 2, 1
+        };
+
         public string IntToRoman(int num)
         {
-            string[] symbol = { "MMM", "MM", "M", "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C", "XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X", "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I" };
-            int[] value = { 3000, 2000, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-
             var result = new StringBuilder();
             var index = 0;
             while (num != 0)
